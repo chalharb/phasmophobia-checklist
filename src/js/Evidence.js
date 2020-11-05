ddd1d9dd9824f4244c90492213b96efe52b89403
chalharb <chalharb@gmail.com>
2020-11-05 14:17:51 -0600

    created js logic for adding ghosts and show/hiding ghosts based on checkbox selection

---
+++ src/js/Evidence.js
@@ -0,0 +1,10 @@
+const evidence = {
+    EMF: "EMF Level 5",
+    FINGERPRINTS: "Fingerprints",
+    FREEZING: "Freezing Temperatures",
+    ORB: "Ghost Orb",
+    WRITING: "Ghost Writing",
+    BOX: "Spirit Box"
+}
+
+export default evidence;
---
+++ src/js/Ghost.js
@@ -0,0 +1,10 @@
+class Ghost {
+    constructor(name, evidence, strengths, weaknesses) {
+        this.name = name;
+        this.evidence = evidence;
+        this.strengths = strengths;
+        this.weaknesses = weaknesses;
+    }
+}
+
+export default Ghost;
